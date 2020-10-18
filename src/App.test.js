a2a5eb0b57ae289862247c63a31f8c3f57839409
MIKE432 <michal.raszczuk.apusoft@gmail.com>
2020-10-18 17:36:41 +0200

    final version

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,61 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import App from './App'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import toJson from 'enzyme-to-json'
+import RouterComponent from './core/routes/router'
+import { FormComponent } from './ui/components/FormComponent'
+import VolumeComponent from './ui/components/VolumeComponent'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() })
+
+const appWrapper = shallow(<App />)
+
+it('should render <App />', () => {
+  expect(appWrapper)
+})
+
+
+it('app renders RouterComponent', () => {
+  expect(
+    appWrapper.find(RouterComponent).length,
+  ).toBe(1);
 });
+
+describe('<FormComponent />', () => {
+  it('should render', () => {
+    const wrapper = shallow(<FormComponent/>)
+    expect(wrapper)
+  })
+
+  it('should match snapshot', () => {
+    const wrapper = shallow(<FormComponent/>)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('First child of FormComponent is div ', () => {
+    const wrapper = shallow(<FormComponent />)
+  
+    expect(wrapper.first().type()).toBe('div')
+  })
+  
+})
+
+describe('<VolumeComponent />', () => {
+
+  it('should render', () => {
+    const wrapper = shallow(<VolumeComponent/>)
+    expect(wrapper)
+  })
+
+  it('should match snapshot', () => {
+    const wrapper = shallow(<VolumeComponent/>)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('First child of VolumeComponent is div ', () => {
+    const wrapper = shallow(<VolumeComponent  />)
+  
+    expect(wrapper.first().type()).toBe('div')
+  })   
+})
